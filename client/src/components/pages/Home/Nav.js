91f3160605bb7a89fa62e284c7b13ba598e956e2
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-05 17:32:02 -0500

    stack nav

--- client/src/components/pages/Home/Nav.js
+++ client/src/components/pages/Home/Nav.js
@@ -5,7 +5,7 @@ import './home.css';
 
 const Nav = () => (
     
-    <div className="ui inverted secondary pointing menu">
+    <div className="ui inverted secondary pointing stackable menu">
         <div className="ui container">
             <Link  className="ui item" to={"/"}>Home</Link>
             <div className="right menu">
