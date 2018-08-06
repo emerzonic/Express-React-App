7fce724b11c4818e7b1e54b53e7e5623b80902e3
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-05 20:12:20 -0500

    build

--- client/src/components/pages/Home/Footer.js
+++ client/src/components/pages/Home/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './home.css';
 
 const Footer = () => (
-    <div className="ui inverted bottom fixed menu">
+    <div className="ui bottom fixed menu">
         <p className="item">Built with React Js - 2018 Emerson Doyah Projects</p>
     </div> 
 );
