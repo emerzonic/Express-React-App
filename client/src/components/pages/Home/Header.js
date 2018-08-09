e56a8e8903679e9ebc433e588a128f0f28c640c2
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-09 12:13:37 -0500

    changed header

--- client/src/components/pages/Home/Header.js
+++ client/src/components/pages/Home/Header.js
@@ -5,7 +5,7 @@ import './home.css';
 const Header = () => (
     <div className="ui raised segment">
         <h1 className="homeTitle">(ReactJS)  Newsopedia</h1>
-        <h2 className="subTitle"> The New York Times Article Scrubber.</h2>
+        <h2 className="subTitle"> Get the latest news from around the world.</h2>
         <h3 className="headnotes">This is a MERN application built using MongoBD 
         database, Express js for server-side routing, React Js for client-side rendering 
         and Node js framework. Search for and save articles of interest.</h3>
