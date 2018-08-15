c82002673ad485b0aed1f0fc1d569ce73e80fb93
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-14 21:24:38 -0500

    new

--- client/src/components/pages/Home/Header.js
+++ client/src/components/pages/Home/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
         <h1 className="homeTitle">(ReactJS)  Newsopedia</h1>
         <h2 className="subTitle"> Get the latest news from around the world.</h2>
         <h3 className="headnotes">This is a MERN application built using MongoBD 
-        database, Express js for server-side routing, React Js for client-side rendering 
+        for database, Express js for server-side routing, React Js for client-side rendering 
         and Node js framework. Search for and save articles of interest.</h3>
         <div className="ui inverted divider"></div>
         {localStorage.getItem('erapp_user')?
