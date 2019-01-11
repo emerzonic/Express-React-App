48afbc8246dd8b476cae1643e14728a88d112fb7
Emerson Doyah <emerson.doyah@gmail.com>
2019-01-10 23:14:43 -0600

    update daynamic date

--- client/src/components/pages/Home/Footer.js
+++ client/src/components/pages/Home/Footer.js
@@ -3,7 +3,7 @@ import './home.css';
 
 const Footer = () => (
     <div className="ui bottom fixed menu">
-        <span className="item">Built with Express & React Copyright &copy;2018 Emerson Doyah Projects</span>
+        <span className="item">Built with Express & React Copyright &copy;{new Date().getFullYear()} Emerson Doyah Projects</span>
     </div> 
 );
 
